4aa17b77646297988fea8297d68f4d83356fab75
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-12-09 12:45:11 -0700

    updated query and started wordcloud code

--- script.js
+++ script.js
@@ -1,59 +1,106 @@
-// request url: https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=your-api-key
+//style scroll nav function
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "5px 10px";
+  } else {
+      document.getElementById("navbar").style.padding = "20px 10px";
+  } 
+};
+
+//call to DictionaryAPI
+// example request url: https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=your-api-key
 
 'use strict';
 
-// put your own value below!
 const apiKey = 'cf8a4365-58c4-4c84-8501-49e65418abc2'; 
 const baseUrl = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/';
 
 function eventSubmit() {
-    $('form').submit(event => {
-      event.preventDefault();
-      console.log('submitEvent ran');
-      const kWord = $('#js-keyword').val();
-      const type = $('input:checked').val();
-      getParks(kWord);
-    });
-}
+  $('form').submit(event => {
+    event.preventDefault();
+    console.log('submitEvent ran');
+    const keyWord = $('#js-keyword').val();
+    //const speech = $('input:checked').val();
+    //const type = $('input:checked').val();
+    getWords(keyWord);
+  });
+};
 
 function formatQueryParams(params) {
   const queryParams = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryParams.join('&');
-}
+};
 
-function getParks(kWord) {
-    const params = {
-      Keyword: kWord,
-    };
-    const queryString = formatQueryParams(params)
-    const url = baseUrl + queryString + '?' +'key=' + apiKey
-    console.log(queryString);
-    console.log(url);
+function getWords(keyWord) {
+  //const query = formatQueryParams(search)
+  const url = baseUrl + keyWord + '?' +'key=' + apiKey
+  console.log(keyWord);
+  console.log(url);
   
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(response.statusText);
-      })
-      .then(responseJson => displayResults(responseJson))
-      .catch(err => {
-        $('#js-definition').text(`Something went wrong: ${err.message}`);
-    });
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayDefinition(responseJson, keyWord)) //, anychart(responseJson)) 
+    //.then(responseJson => console.log(responseJson))
+    .catch(err => {
+      $('#js-definition').text(`Something went wrong: ${err.message}`);
+  });
 }
 
-function displayResults(responseJson) {
+//display definition
+function displayDefinition(responseJson, keyWord) {
+  console.log(keyWord);
   console.log(responseJson);
+  console.log(responseJson.length)
   $('#js-definition').empty();
-    $('#js-definition').append(
-        `<h2>Definition</h2>
-        <p>${meta.id}</p>
-        <p>${meta.def.dt}</p>`
-    )};
-    $('#js-keyword').val('');
-    $('#input:checked').val('');
+  for (let i = 0; i < responseJson.length; i++)
+    if (keyWord === responseJson.meta.id) {
+      $('#js-definition').append(
+        `<a id="definition"> </a>
+          <h2>Definition:</h2>
+          <p>${responseJson.hwi.hw}</p>
+          <p>${responseJson.shortdef}</p>`
+      );
+    }
+    
+  $('#js-keyword').val('');
+  $('#input:checked').val('');
 };
 
 $(eventSubmit);
+
+//create word cloud
+function anychart(responseJson) {
+  let data = [
+    //example data{"x": "word", "value": 0, category: "synonym"}
+  ];
+  let chart = anychart.tagCloud(data);
+  $('#js-definition').empty();
+  for (let i = 0; i < allOptions.options.length; i++) {
+    $('.insert-option').append(
+      `<section id="js-cloud">
+        <a id="word-cloud"> </a>
+        <h2>Word Cloud:</h2>
+        
+      </section>`)};
+
+// set a chart title
+chart.title('15 most spoken languages')
+// set an array of angles at which the words will be laid out
+chart.angles([0])
+// enable a color range
+chart.colorRange(true);
+// set the color range length
+chart.colorRange().length('80%');
+
+// display the word cloud chart
+chart.container("container");
+chart.draw();
+};
