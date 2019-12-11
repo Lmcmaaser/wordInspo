d698fa4db9f67e2822903e6c3c684cb8ff23a130
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-12-11 13:19:47 -0700

    function error

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ function am4core(concat) {
   data.push(concat);
   console.log(data);
   //add data
-  series.data = data
+  series.data =  //needs to consist of an array of objects
 
   series.dataFields.word = "tag";
   series.dataFields.value = "weight"; 
@@ -124,3 +124,44 @@ function am4core(concat) {
   series.colors.passOptions = {};
 }; 
 $(eventSubmit);
+
+
+/*const feelings =  ["content", "contented", "gratified", "pleased", "satisfied"] 
+
+const things = feelings.map((feeling, index) => {
+  return {
+   tag: feeling,
+   index: index + 1,
+  }
+});
+
+ex. series.data = [
+        {
+            "tag": "Breaking News",
+            "weight": 60
+        }, {
+            "tag": "Environment",
+            "weight": 80
+        }, {
+            "tag": "Politics",
+            "weight": 90
+        }, {
+            "tag": "Business",
+            "weight": 25
+        }, {
+            "tag": "Lifestyle",
+            "weight": 30
+        }, {
+            "tag": "World",
+            "weight": 45
+        }, {
+            "tag": "Sports",
+            "weight": 160
+        }, {
+            "tag": "Fashion",
+            "weight": 20
+        }, {
+            "tag": "Education",
+            "weight": 78
+        }
+    ];
